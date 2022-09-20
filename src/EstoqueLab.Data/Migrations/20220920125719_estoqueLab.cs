f1c0bea5bb5e3aaa95f68082966c5393599d37ba
Thiago Mateus <thiago.mateus@tlsistemas.com.br>
2022-09-20 09:59:15 -0300

    Adicionando migrations

---
+++ src/EstoqueLab.Data/Migrations/20220920125719_estoqueLab.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EstoqueLab.Data.Migrations
+{
+    public partial class estoqueLab : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Ativo = table.Column<bool>(type: "tinyint(1)", nullable: false, defaultValue: true),
+                    CriadoEm = table.Column<DateTime>(type: "datetime(6)", nullable: false, defaultValue: new DateTime(2022, 9, 20, 9, 57, 18, 922, DateTimeKind.Local).AddTicks(6839)),
+                    AtualizadoEm = table.Column<DateTime>(type: "datetime(6)", nullable: false, defaultValue: new DateTime(2022, 9, 20, 9, 57, 18, 922, DateTimeKind.Local).AddTicks(7099))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Valor = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    CategoriaId = table.Column<int>(type: "int", nullable: false),
+                    Ativo = table.Column<bool>(type: "tinyint(1)", nullable: false, defaultValue: true),
+                    CriadoEm = table.Column<DateTime>(type: "datetime(6)", nullable: false, defaultValue: new DateTime(2022, 9, 20, 9, 57, 18, 922, DateTimeKind.Local).AddTicks(8118)),
+                    AtualizadoEm = table.Column<DateTime>(type: "datetime(6)", nullable: false, defaultValue: new DateTime(2022, 9, 20, 9, 57, 18, 922, DateTimeKind.Local).AddTicks(8436))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
